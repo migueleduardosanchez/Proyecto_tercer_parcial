7ecf384f4371a91177e7338fde0638b210690524
fairygarbage <joseriavila@gmail.com>
2023-05-22 18:52:42 -0600

    Se agrega el boton del link del cv de la matricula 345635

--- app/src/main/java/mx/uach/fing/proyectotercerparcial/MainActivity.java
+++ app/src/main/java/mx/uach/fing/proyectotercerparcial/MainActivity.java
@@ -2,13 +2,32 @@ package mx.uach.fing.proyectotercerparcial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button _btn_link2;
+    String _url = "https://fairygarbage.github.io/CV/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        _btn_link2 = findViewById(R.id.btn_link2);
+        _btn_link2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri _link = Uri.parse(_url);
+                Intent i = new Intent(Intent.ACTION_VIEW, _link);
+            }
+        });
     }
 }
