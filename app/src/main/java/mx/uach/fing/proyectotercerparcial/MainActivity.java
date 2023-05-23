528177973c99acd354e34c268f6b57a745383049
Miguel Sánchez <a324795@uach.mx>
2023-05-23 16:54:00 -0600

    boton 324795

--- app/src/main/java/mx/uach/fing/proyectotercerparcial/MainActivity.java
+++ app/src/main/java/mx/uach/fing/proyectotercerparcial/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,7 +14,10 @@ import java.net.URI;
 public class MainActivity extends AppCompatActivity {
 
     private Button _btn_link2;
+    private Button _a324795;
+
     String _url = "https://fairygarbage.github.io/CV/";
+    String _url324795 = "https://migueleduardosanchez.github.io/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +29,19 @@ public class MainActivity extends AppCompatActivity {
         _btn_link2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Uri _link = Uri.parse(_url);
-                Intent i = new Intent(Intent.ACTION_VIEW, _link);
+                Uri _link2 = Uri.parse(_url);
+                Intent i = new Intent(Intent.ACTION_VIEW, _link2);
+            }
+        });
+
+        _a324795 = findViewById(R.id.a324795);
+        _a324795.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                //Uri _link1 = Uri.parse(_url324795);
+                Log.d("click", "ok");
+                Intent i = new Intent(Intent.ACTION_VIEW, _link1);
             }
         });
     }
+
 }
